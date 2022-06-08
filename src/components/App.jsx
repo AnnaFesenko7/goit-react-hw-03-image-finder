f027b0cad11ceecedf98605eb21bc694ca8c7dd0
AnnaFesenko7 <annafecenko7@gmail.com>
2022-06-08 20:18:22 +0200

    modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import s from './App';
+import Modal from 'components/Modal';
+
+export default class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  componentDidMount() {
+    const prevTodos = localStorage.getItem('todos');
+    const parsTodos = JSON.parse(prevTodos);
+    if (parsTodos) {
+      this.setState({ todos: parsTodos });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (prevState.todos !== this.state.todos) {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    }
+  }
+
+  toggleModal = () => {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+    }));
+  };
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div className={s.App}>
+        <button onClick={this.toggleModal} type="button">
+          Открыть
+        </button>
+        {showModal && <Modal onClose={this.toggleModal}></Modal>}
+      </div>
+    );
+  }
+}
